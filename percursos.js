903213cd80763ea3a99a39b3cc7fb3132e39983d
benylandau <benylandau@gmail.com>
2025-06-03 22:04:03 -0300

    Add files via upload

---
+++ percursos.js
@@ -0,0 +1,1 @@
+// Lista de percursos carregada automaticamente
--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-
-function showMedia(src) {
-    const viewer = document.getElementById('mediaViewer');
-    const content = document.getElementById('mediaContent');
-    content.src = src;
-    viewer.classList.add('show');
-}
-
-function closeMedia() {
-    const viewer = document.getElementById('mediaViewer');
-    const content = document.getElementById('mediaContent');
-    content.src = "";
-    viewer.classList.remove('show');
-}
+// Script para abrir imagens e mostrar legendas
